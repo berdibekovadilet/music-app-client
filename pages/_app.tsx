f9a213d04cc4e319041b4f8ab942d0478331ebc8
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-25 21:55:44 +0600

    change _app.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from "react";
+import { Provider } from "react-redux";
 import { AppProps } from "next/app";
 import { wrapper } from "../store";
 import "../styles/globals.css";
 
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-);
+const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
+};
 
-export default wrapper.withRedux(WrappedApp);
+export default MyApp;
